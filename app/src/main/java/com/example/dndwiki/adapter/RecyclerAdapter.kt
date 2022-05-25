a5f40b07938ef806c39defcd42cc13a7b0243452
Roy Suttie <roy.suttie@klm.com>
2022-05-25 11:00:37 +0200

    data pipeline fakeNetwork -> Fragment

--- app/src/main/java/com/example/dndwiki/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/dndwiki/adapter/RecyclerAdapter.kt
@@ -1,53 +1,25 @@
 package com.example.dndwiki.adapter
 
+
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dndwiki.R
-import com.example.dndwiki.data.DamageType
-import com.example.dndwiki.data.School
 import com.example.dndwiki.data.Spell
-import com.example.dndwiki.home.HomeViewModel
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    val spells = listOf(
-        Spell("1 action",
-            "Wizard, Sorcerer",
-            DamageType("Acid"),
-            "Instantaneous",
-            "acid-splash",
-            0,
-            "Acid Splash",
-            School("Conjuration"),
-            "/api/spells/acid-splash",
-            "You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a dexterity saving throw or take 1d6 acid damage."),
+    private var data: List<Spell> = emptyList()
 
-        Spell("1 bonus action",
-            "Cleric",
-            DamageType("Force"),
-            "1 minute",
-            "spiritual-weapon",
-            2,
-            "Spiritual Weapon",
-            School("Evocation"),
-            "/api/spells/spiritual-weapon",
-            "You create a floating, spectral weapon within range that lasts for the duration or until you cast this spell again. When you cast the spell, you can make a melee spell attack against a creature within 5 feet of the weapon. On a hit, the target takes force damage equal to 1d8 + your spellcasting ability modifier."),
+    fun setSpells(list: List<Spell>) {
 
-        Spell("1 action",
-            "Sorcerer, Warlock, Wizard",
-            DamageType("Necrotic"),
-            "Instantaneous",
-            "circle-of-death",
-            6,
-            "Circle of Death",
-            School("Necromancy"),
-            "/api/spells/circle-of-death",
-            "A sphere of negative energy ripples out in a 60-foot radius sphere from a point within range. Each creature in that area must make a constitution saving throw. A target takes 8d6 necrotic damage on a failed save, or half as much damage on a successful one.")
-    )
+        data = list
+
+         notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
@@ -56,12 +28,12 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return spells.size
+        return data.size
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, i: Int) {
 
-        holder.bind(spells[i])
+        holder.bind(data[i])
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/example/dndwiki/spellFinder/SpellFinderViewModel.kt
+++ app/src/main/java/com/example/dndwiki/spellFinder/SpellFinderViewModel.kt
@@ -1,11 +1,24 @@
 package com.example.dndwiki.spellFinder
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.recyclerview.widget.RecyclerView
-import com.example.dndwiki.adapter.RecyclerAdapter
+import com.example.dndwiki.data.Spell
+import com.example.dndwiki.network.FakeNetwork
 
 
 class SpellFinderViewModel : ViewModel() {
+
+    private val _spellsList = MutableLiveData<List<Spell>>()
+    val spellsList: LiveData<List<Spell>>
+        get() = _spellsList
+
+    fun getSpells() {
+        val fakeNetwork = FakeNetwork()
+        val spells = fakeNetwork.getSpells()
+
+        _spellsList.value = spells
+    }
 
 }
 
