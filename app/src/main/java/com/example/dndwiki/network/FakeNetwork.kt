c816a3fdbd4768cf94b45614be1a013925261d92
Roy Suttie <roy.suttie@klm.com>
2022-05-27 14:49:48 +0200

    Search bar function added

--- app/src/main/java/com/example/dndwiki/network/FakeNetwork.kt
+++ app/src/main/java/com/example/dndwiki/network/FakeNetwork.kt
@@ -1,6 +1,5 @@
 package com.example.dndwiki.network
 
-import androidx.lifecycle.MutableLiveData
 import com.example.dndwiki.data.DamageType
 import com.example.dndwiki.data.School
 import com.example.dndwiki.data.Spell
@@ -8,7 +7,8 @@ import com.example.dndwiki.data.Spell
 class FakeNetwork {
 
     private var spells = listOf(
-        Spell("1 action",
+        Spell(
+            "1 action",
             "Wizard, Sorcerer",
             DamageType("Acid"),
             "Instantaneous",
@@ -17,9 +17,11 @@ class FakeNetwork {
             "Acid Splash",
             School("Conjuration"),
             "/api/spells/acid-splash",
-            "You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a dexterity saving throw or take 1d6 acid damage."),
+            "You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a dexterity saving throw or take 1d6 acid damage."
+        ),
 
-        Spell("1 bonus action",
+        Spell(
+            "1 bonus action",
             "Cleric",
             DamageType("Force"),
             "1 minute",
@@ -28,9 +30,11 @@ class FakeNetwork {
             "Spiritual Weapon",
             School("Evocation"),
             "/api/spells/spiritual-weapon",
-            "You create a floating, spectral weapon within range that lasts for the duration or until you cast this spell again. When you cast the spell, you can make a melee spell attack against a creature within 5 feet of the weapon. On a hit, the target takes force damage equal to 1d8 + your spellcasting ability modifier."),
+            "You create a floating, spectral weapon within range that lasts for the duration or until you cast this spell again. When you cast the spell, you can make a melee spell attack against a creature within 5 feet of the weapon. On a hit, the target takes force damage equal to 1d8 + your spellcasting ability modifier."
+        ),
 
-        Spell("1 action",
+        Spell(
+            "1 action",
             "Sorcerer, Warlock, Wizard",
             DamageType("Necrotic"),
             "Instantaneous",
@@ -39,7 +43,21 @@ class FakeNetwork {
             "Circle of Death",
             School("Necromancy"),
             "/api/spells/circle-of-death",
-            "A sphere of negative energy ripples out in a 60-foot radius sphere from a point within range. Each creature in that area must make a constitution saving throw. A target takes 8d6 necrotic damage on a failed save, or half as much damage on a successful one.")
+            "A sphere of negative energy ripples out in a 60-foot radius sphere from a point within range. Each creature in that area must make a constitution saving throw. A target takes 8d6 necrotic damage on a failed save, or half as much damage on a successful one."
+        ),
+
+        Spell(
+            "1 action",
+            "Sorcerer, Warlock, Wizard",
+            DamageType("Poison"),
+            "Instantaneous",
+            "poison-spray",
+            0,
+            "Poison Spray",
+            School("Conjuration"),
+            "/api/spells/poison-spray",
+            "You extend your hand toward a creature you can see within range and project a puff of noxious gas from your palm. The creature must succeed on a constitution saving throw or take 1d12 poison damage."
+        )
     )
 
     fun getSpells(): List<Spell> {
