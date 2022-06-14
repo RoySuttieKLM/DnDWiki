d51891ee52eebf524653465ae5310c35beab75b7
Roy Suttie <roy.suttie@klm.com>
2022-06-14 13:24:30 +0200

    startKoin {} (accidental wrong import)

--- app/src/main/java/com/example/dndwiki/DnDWiki.kt
+++ app/src/main/java/com/example/dndwiki/DnDWiki.kt
@@ -5,13 +5,13 @@ import com.example.dndwiki.diModule.DnDModule
 import com.example.dndwiki.diModule.ViewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext
+import org.koin.core.context.GlobalContext.startKoin
 
 class DnDWiki : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        GlobalContext.startKoin {
+        startKoin {
             androidLogger()
             androidContext(this@DnDWiki)
             modules(DnDModule, ViewModelModule)
